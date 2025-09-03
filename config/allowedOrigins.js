a6ed8b3ff62d79fe4448233983f35a03e600c4c3
Ernest Lin <linchongtik@gmail.com>
2025-09-02 21:40:53 -0700

    add origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://funny-sopapillas-7d6b8a.netlify.app",
+];
 
 module.exports = allowedOrigins;
