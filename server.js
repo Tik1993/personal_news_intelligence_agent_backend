4865e0a6aea2d1db7597dbe384f5ea94bccde86b
Ernest Lin <linchongtik@gmail.com>
2025-09-02 17:21:06 -0700

    connect DB

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const corsOptions = require("./config/corsOptions");
 
 const app = express();
-app.use(cors(corsOptions));
 
-app.listen(3000, () => {
-  console.log("listening in port 3000");
+const PORT = 3000;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+  } catch (err) {
+    console.log(err);
+  }
+}
+connectDB();
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDb");
+  app.listen(PORT, () => {
+    console.log(`listening in port ${PORT}`);
+  });
 });
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Welcome to my news agent api");
