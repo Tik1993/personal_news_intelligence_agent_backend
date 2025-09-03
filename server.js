408b96f7a02b659230220639765062fffbc6d410
Ernest Lin <linchongtik@gmail.com>
2025-09-02 17:09:58 -0700

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
+const corsOptions = require("./config/corsOptions");
 
 const app = express();
+app.use(cors(corsOptions));
 
 app.listen(3000, () => {
   console.log("listening in port 3000");
